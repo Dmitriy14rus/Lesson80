dc98fb29c49ed791a7603ea3f25eced2c4dfedad
Dmitriy14rus <129680732+Dmitriy14rus@users.noreply.github.com>
2023-07-29 22:06:44 +0900

    Add files via upload

---
+++ docs/main.js
@@ -0,0 +1,25 @@
+//1)============================================================ 
+let firstNumber = 2
+let endNumber = 12
+let evenNumber = 2
+
+for (firstNumber; firstNumber <= endNumber; firstNumber = firstNumber + evenNumber) {
+    console.log(firstNumber)
+}
+// 2)=====================================================
+for (let i = 10; i >= 1; i = i - 1) {
+    console.log(i)
+}
+// 3)====================================================
+let x = null
+
+while (x <= 10) {
+    x = x + 2
+    console.log(x)
+}
+
+let y = 10
+while (y >= 1) {
+    y = y - 1
+    console.log (y+1)
+}
